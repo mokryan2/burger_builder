b508050b2944e1347893d3a7d2f6f9a890b1b4bd
Ryan Mok <mokryan17@gmail.com>
2019-06-24 16:07:54 -0500

    Added a Layout.css file

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.css";
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, Side Drawer, and Backdrop will eventually go here</div>
-        <p>{props.children}</p>
+        <p className={classes.Content}>{props.children}</p>
     </Aux>
 );
 
