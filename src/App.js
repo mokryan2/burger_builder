cf9b548cf3db835dd9912e02f25853b099274417
Ryan Mok <mokryan17@gmail.com>
2019-06-24 15:33:58 -0500

    Converted App.js into a class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>testy test</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>testy test</h1>
+        <p>is this still running?</p>
+      </div>
+    );
+  }
 }
 
 export default App;
