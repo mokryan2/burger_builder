7d545e37dff87639dee18ec99daac4e36de95a77
Ryan Mok <mokryan17@gmail.com>
2019-06-24 16:50:16 -0500

    Finished adding the rest of the ingredients

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -9,18 +9,27 @@ const burgerIngredient = (props) => {
             ingredient = <div className={classes.BreadBottom}></div>
             break;
         case ("bread-top"):
-            ingredient = <div className={classes.BreadBottom}></div>
+            ingredient = <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>
+            </div>
             break;
         case ("meat"):
-            ingredient = <div className={classes.BreadBottom}></div>
+            ingredient = <div className={classes.Meat}></div>
             break;
         case ("cheese"):
-            ingredient = <div className={classes.BreadBottom}></div>
+            ingredient = <div className={classes.Cheese}></div>
             break;
         case ("bacon"):
-            ingredient = <div className={classes.BreadBottom}></div>
+            ingredient = <div className={classes.Bacon}></div>
             break;
+        case ("lettuce"):
+            ingredient = <div className={classes.Lettuce}></div>
+            break;
+        default:
+            ingredient = null;
     }
+    return ingredient;
 };
 
 export default burgerIngredient;
