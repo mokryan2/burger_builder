5b5614a407dc177e2fd58bab1d613ad013e19fca
Ryan Mok <mokryan17@gmail.com>
2019-06-24 16:42:22 -0500

    Added initial set up for burger images in css

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,7 +1,26 @@
 import React from "react";
+import classes from "./BurgerIngredients.css"
 
 const burgerIngredient = (props) => {
     let ingredient = null;
+
+    switch (props.type) {
+        case ("bread-bottom"):
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+        case ("bread-top"):
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+        case ("meat"):
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+        case ("cheese"):
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+        case ("bacon"):
+            ingredient = <div className={classes.BreadBottom}></div>
+            break;
+    }
 };
 
 export default burgerIngredient;
