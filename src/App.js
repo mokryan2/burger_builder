5671eab38ecdb9236aa155d6bfcb0517ba919a25
Ryan Mok <mokryan17@gmail.com>
2019-06-24 15:54:36 -0500

    Created higher order component to allow easier wrapping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>testy test</h1>
-        <p>is this still running?</p>
+        <Layout>
+          <h1>testy test</h1>
+          <p>is this still running?</p>
+        </Layout>
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Aux from "../../hoc/Auxiliary";
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, Side Drawer, and Backdrop will eventually go here</div>
+    </Aux>
+);
+
+export default layout
