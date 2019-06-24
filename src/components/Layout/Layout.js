745ab9cfbbf9d5309efce312bbb4196506d8f0fc
Ryan Mok <mokryan17@gmail.com>
2019-06-24 15:55:45 -0500

    Forgot to run props.children in layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,6 +4,7 @@ import Aux from "../../hoc/Auxiliary";
 const layout = (props) => (
     <Aux>
         <div>Toolbar, Side Drawer, and Backdrop will eventually go here</div>
+        <p>{props.children}</p>
     </Aux>
 );
 
