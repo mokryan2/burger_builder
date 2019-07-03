0406af58fb8f4e50d17d7d7c5efe8f5201395ab2
Ryan Mok <mokryan17@gmail.com>
2019-07-03 00:04:35 -0500

    Added base code for ingredient buttons

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./BuildControls"
+
+const buidControls = (props) => (
+    <div className={classes.BuildControls}>
+        <button>Meat</button>
+        <button>cheese</button>
+    </div>
+)
+
+
+
+export default buidControls;
