31dd17949feaefd9358fa3daab1f796baa1bea17
Ryan Mok <mokryan17@gmail.com>
2019-07-03 00:27:21 -0500

    Displaying Add/Remove buttons, but not the actual ingredient...

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,20 @@
 import React from "react";
+import BuildControl from "./BuildControl/BuildControl"
 import classes from "./BuildControls.css"
+
+const controls = [
+    { label: "Lettuce", type: "lettuce" },
+    { label: "Meat", type: "meat" },
+    { label: "Cheese", type: "cheese" },
+    { label: "Bacon", type: "bacon" },
+
+];
 
 const buidControls = (props) => (
     <div className={classes.BuildControls}>
-        <button>Meat</button>
-        <button>cheese</button>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
 )
 
