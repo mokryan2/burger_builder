51575a0a81aee9742fbea6f6d61010cdb8ae3e0f
Ryan Mok <mokryan17@gmail.com>
2019-07-04 22:56:51 -0500

    Added addIngredientHandler logic

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls"
 
+const INGREDIENT_PRICE = {
+    lettuce: .5,
+    bacon: 1,
+    cheese: .75,
+    meat: 1.5
+}
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,8 +18,25 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 5
     };
+
+    addIngredeientHandler = (type) => {
+        const oldIngredientCount = this.state.ingredients[type];
+        const updatedIngredientCount = oldIngredientCount + 1;
+        const updatedIngredient = {
+            ...this.state.ingredients
+        };
+        updatedIngredient[type] = updatedIngredientCount;
+        const priceAddition = INGREDIENT_PRICE[type]
+        const startPrice = this.state.totalPrice;
+        const endPrice = startPrice + priceAddition;
+        this.setState({
+            totalPrice: endPrice,
+            ingredients: updatedIngredientCount
+        })
+    }
 
     render() {
         return (
