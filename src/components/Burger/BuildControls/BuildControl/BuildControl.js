0553c264678817f69485c0d718c98264ee72f26d
Ryan Mok <mokryan17@gmail.com>
2019-07-04 23:29:06 -0500

    Finalized disable of remove button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -11,6 +11,7 @@ const buildControl = (props) => (
         <button
             className={classes.Remove}
             onClick={props.removed}
+            disabled={props.disabled}
         >Remove</button>
     </div>
 )
