74112b2c96e4d2feee656e52898a36176061e6c2
Ryan Mok <mokryan17@gmail.com>
2019-07-08 23:20:11 -0500

    Added logic to display ingredient names and count

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,14 +3,20 @@ import Aux from "../../../hoc/Auxiliary"
 
 const orderSummary = (props) => {
 
-    const ingredientSummary = props.ingredients
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li>
+                    <span style={{ textTransform: "capitalize" }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>)
+        })
 
     return (
         <Aux>
             <h3>Your Order</h3>
             <p>You've selected the following additions to your burger:</p>
             <ul>
-
+                {ingredientSummary}
             </ul>
         </Aux>
     );
