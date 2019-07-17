c5c8a2ddebb0f665b75f3bec4809c46b7f9e7637
Ryan Mok <mokryan17@gmail.com>
2019-07-16 23:45:23 -0500

    Added stand-in code for toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const toolbar = (props) => ();
+const toolbar = (props) => (
+    <header>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
 
 export default toolbar;
