ae57bfe793f712b102fa50c378e8ef35f0e006e8
Ryan Mok <mokryan17@gmail.com>
2019-07-19 08:49:21 -0500

    Added in logo and navItems to sideDrawer

--- src/components/Navigation/NavItems/SideDrawer/SideDrawer.js
+++ src/components/Navigation/NavItems/SideDrawer/SideDrawer.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Logo from "../../../Logo/Logo";
+import NavItems from "../NavItems"
 import classes from "./SideDrawer.css";
 
-const sideDrawer = (props) => (
-    <div>
-
-    </div>
-);
+const sideDrawer = (props) => {
+    return (
+        <div>
+            <Logo />
+            <nav>
+                <NavItems />
+            </nav>
+        </div>
+    )
+};
 
 export default sideDrawer;
