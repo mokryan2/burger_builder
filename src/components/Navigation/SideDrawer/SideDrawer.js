946dad3153e45ca6b5421307e09d3e3c4d7db73d
Ryan Mok <mokryan17@gmail.com>
2019-07-21 23:02:33 -0500

    Animation for sidedrawer to open and close works

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,13 +6,18 @@ import Aux from "../../../hoc/Auxiliary";
 import classes from "./SideDrawer.css";
 
 const sideDrawer = (props) => {
+    let attachClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachClasses = [classes.SideDrawer, classes.Open];
+    };
+
     return (
         <Aux>
             <Backdrop
                 show={props.open}
                 clicked={props.closed}
             />
-            <div className={classes.SideDrawer}>
+            <div className={attachClasses.join(" ")}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
