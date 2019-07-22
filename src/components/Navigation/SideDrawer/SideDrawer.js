92c5040d9a3168db4c527d626b33df0730b008cc
Ryan Mok <mokryan17@gmail.com>
2019-07-21 22:25:05 -0500

    Implemented the backdrop to the sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,25 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavItems from "../NavItems/NavItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxiliary";
 import classes from "./SideDrawer.css";
 
 const sideDrawer = (props) => {
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop 
+                show
+            />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavItems />
+                </nav>
             </div>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>
+        </Aux>
     )
 };
 
