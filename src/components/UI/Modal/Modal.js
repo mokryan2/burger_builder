db4d7955a8150847404aeb128bc6491ac94d97a8
Ryan Mok <mokryan17@gmail.com>
2019-07-24 00:16:34 -0500

    Added lifecycle hooks to modal to confirm changes to performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,6 +4,17 @@ import Aux from "../../../hoc/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    };
+
+    // This will make it so we can check to see if the order summary component will update if the show property should change 
+
+    componentWillUpdate() {
+        console.log("[Modal] WillUpdate")
+    };
+
     render() {
         return (
             <Aux>
