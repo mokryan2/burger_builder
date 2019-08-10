48d51f4ef7b55e55122cb22778d6d65961632df5
Ryan Mok <mokryan17@gmail.com>
2019-08-10 00:07:22 -0500

    Converted withErrorHandler into an anonymous class based component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Auxiliary";
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Aux>
-                <Modal show>
-                    Something is broken (╯°□°)╯︵ ┻━┻
-                </Modal>
-                <WrappedComponent {...props} />
-            </Aux>
-        );
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        // This is made to be an anonymous class b/c the class itself is never actually used.
+        // It's only returned at this point and prety much will just create a bunch of classes for us.
+        render() {
+            return (
+                <Aux>
+                    <Modal show>
+                        Something is broken (╯°□°)╯︵ ┻━┻
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            )
+        }
     }
 }
 
