2497fc45b24d00059ee7084ae54e805fb042ea05
Ryan Mok <mokryan17@gmail.com>
2019-08-09 23:57:53 -0500

    imported aux HOC and modal into error handler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Auxiliary";
 
 const withErrorHandler = (WrappedComponent) => {
     return (props) => {
         return (
-            <WrappedComponent {...props} />
+            <Aux>
+                <Modal>
+            Something is broken (╯°□°)╯︵ ┻━┻
+                </Modal>
+                <WrappedComponent {...props} />
+            </Aux>
         );
     }
 }
