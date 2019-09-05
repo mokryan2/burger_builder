7eb07b3a7395cd7f82695c1f8acb4ca942c621d1
Ryan Mok <mokryan17@gmail.com>
2019-09-04 23:24:32 -0500

    Implemented Button for cancel/continue and created base styling for main component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
 import classes from "./CheckoutSummary.css"
 
 const checkoutSummary = (props) => {
     return (
-        <div>
+        <div className={classes.CheckoutSummary}>
             <h1>You sure have some interesting tastes...</h1>
             <div className={classes.Burger}>
-                <Burger />
+                <Burger
+                    ingredients={props.ingredients}
+                />
             </div>
+            <Button
+                btnType="Danger"
+                clicked>
+                CANCEL</Button>/>
+            <Button
+                btnType="Success"
+                clicked>
+                CONTINUE</Button>/>
+
         </div>
     )
 };
