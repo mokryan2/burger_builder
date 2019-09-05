ed99b8fbf93d133cb38a40fb769754e3cd2afdee
Ryan Mok <mokryan17@gmail.com>
2019-09-04 23:17:15 -0500

    Added in some basic styling to contain the Burger component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Burger from "../../Burger/Burger";
+import classes from "./CheckoutSummary.css"
 
-const checkoutSummary = (props) =>{
-
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>You sure have some interesting tastes...</h1>
+            <div className={classes.Burger}>
+                <Burger />
+            </div>
+        </div>
+    )
 };
 
 export default checkoutSummary;
