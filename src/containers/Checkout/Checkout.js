ed956513386c5c2405e81dffb695e38b5bc7a7f8
Ryan Mok <mokryan17@gmail.com>
2019-09-06 09:42:02 -0500

    The checkout cancelled handler now pushes the history back to the burgerbuilder

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,13 +12,23 @@ class Checkout extends Component {
             cheese: 1
         }
     }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.replace("/checkout/contact-data");
+    }
+
+
     render() {
         return (
             <div>
                 <CheckoutSummary
                     ingredients={this.state.ingredients}
-                    checkoutCancelled={}
-                    checkoutContinued={}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}
                 />
             </div>
         )
