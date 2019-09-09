484353de6a79d9492d87cb88244a111686a90de2
Ryan Mok <mokryan17@gmail.com>
2019-09-09 18:07:21 -0500

    Made a temporary form for contact information

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
 
 class ContactData extends Component {
     state = {
-
+        name: "",
+        email: "",
+        address: {
+            street: "",
+            zipCode: ""
+        },
+        phoneNumber: ""
     };
-    
-    render() {
-        return ()
 
+    render() {
+        return (
+            <div>
+                <h4>Where we dropping this off?</h4>
+                <form>
+                    <input type="text" name="name" placeholder="Your Name" />
+                    <input type="email" name="email" placeholder="Your E-mail" />
+                    <input type="text" name="street" placeholder="Your Address" />
+                    <input type="text" name="zipCode" placeholder="Your Zip Code" />
+                    <input type="text" name="phoneNumber" placeholder="Your Phone Number" />
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        )
     }
 };
 
