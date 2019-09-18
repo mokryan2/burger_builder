cb2e6e5c37dce2db1bf9491266b94b8871bc01ae
Ryan Mok <mokryan17@gmail.com>
2019-09-18 00:29:00 -0500

    Ingredients from the order are being displayed in the DOM

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,31 @@
 import React from "react";
 import classes from "./Order.css"
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: meat (1)</p>
-        <p>Price: USD 6.75</p>
-    </div>
-);
+const order = (props) => {
+
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            total: props.ingredients[ingredientName]
+            // Given the ingredients object in the order, we're pushing the name of the ingredient and the total amount of said
+            // ingredient into a new array to allow us to map the ingredients here
+        });
+    };
+
+    const ingredientOutput = ingredients.map(ig => {
+        return (
+            <span key={ig.name}>{ig.name}: {ig.total} </span>
+        )
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD ${props.price}</strong></p>
+        </div>
+    );
+};
 
 export default order;
