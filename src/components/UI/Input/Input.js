c16f007d79f3a605b3b8536ed3bea7b26fbfb5b6
Ryan Mok <mokryan17@gmail.com>
2019-09-18 22:40:59 -0500

    Added styling to the individual parts of the form and set up props to handle different form types

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./Input.css";
 
 const input = (props) => {
 
@@ -6,17 +7,22 @@ const input = (props) => {
 
     switch (props.inputType) {
         case ("input"):
-            inputElement = <input />
+            inputElement = <input className={classes.InputElement}{...props} />
+            break;
+        case ("textarea"):
+            inputElement = <textarea className={classes.InputElement} {...props} />
             break;
         default:
-            inputElement = <input />
+            inputElement = <input className={classes.InputElement} {...props} />
     }
     // Becasue we're going to have multiple types of inputs, it makes sense to use a switch case to manage the different inputs
     // Alternatively, we could have also just made multiple custom components, but this is much faster.
+    // We expect to get the attributes we want as props for the input wrapper; this will allow us to distribute them on the element
 
     return (
-        <div>
-            <label>{props.label}</label>
+        <div className={classes.Input}>
+            <label className={classes.Label}>{props.label}</label>
+            {inputElement}
         </div>
     );
 }
