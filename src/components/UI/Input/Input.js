27a87081e7e1cfff7f1cfc9a1e459f0ed12032ea
Ryan Mok <mokryan17@gmail.com>
2019-09-18 22:27:45 -0500

    Starting switch statement to handle different input fields

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const input = (props) => (
-    <div>
+const input = (props) => {
 
-    </div>
-);
+    let inputElement = null;
+
+    switch (props.inputType) {
+        case ("input"):
+            inputElement = <input />
+            break;
+        default:
+            inputElement = <input />
+    }
+    // Becasue we're going to have multiple types of inputs, it makes sense to use a switch case to manage the different inputs
+    // Alternatively, we could have also just made multiple custom components, but this is much faster.
+
+    return (
+        <div>
+            <label>{props.label}</label>
+        </div>
+    );
+}
 
 export default input;
