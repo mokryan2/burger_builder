6f20ef6db3a7ab1b187bfcfb8d75e8641db3e6db
Ryan Mok <mokryan17@gmail.com>
2019-10-07 10:58:11 -0500

    Connected contact data to reducer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -20,22 +20,13 @@ class Checkout extends Component {
         return (
             <div>
                 <CheckoutSummary
-                    ingredients={this.state.ingredients}
+                    ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
                 <Route
                     path={this.props.match.path + "/contact-data"}
-                    render={(props) => (
-                        <ContactData
-                            ingredients={this.state.ingredients}
-                            price={this.state.totalPrice}
-                            {...props} />)
-                        // We pass props here to give the ContactData component access to the history object so we can eventually 
-                        // redirect the app to the BurgerBuilder after posting the data we created to firebase
-                    }
-                // We changed this from the component to the render method to allow the ContactData component to accept props. 
-                // This is important so the user data can actually be passed and collected.
+                    component={ContactData}
                 />
             </div>
         )
@@ -44,12 +35,11 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.ingredients
     };
 };
 
 export default connect(mapStateToProps)(Checkout);
 // We don't really need to use mapDispatchToProps here b/c we're not passing data; however, it is worth noting
 // that if it were the other way around and we weren't passing mapStateToProps the connect method requires you to pass
-// null, mapDispatchToProps in this order because MDTP needs to always be the second argument
+// (null, mapDispatchToProps) in this order because MDTP needs to always be the second argument
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import classes from "./ContactData.css"
 import axios from "../../../axios-orders";
+import { connect } from "react-redux";
 
 class ContactData extends Component {
     state = {
@@ -126,7 +127,7 @@ class ContactData extends Component {
         // the exact form element to allow collection by firebase!
 
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         };
@@ -230,4 +231,11 @@ class ContactData extends Component {
     }
 };
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
