d4bf12330b62cdc6e80458e0b51b9d96501e55e7
Ryan Mok <mokryan17@gmail.com>
2019-10-23 22:47:34 -0500

    Added in a toFixed in the Order container to account for extended price values

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -25,7 +25,7 @@ const order = (props) => {
     return (
         <div className={classes.Order}>
             <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>USD ${props.price}</strong></p>
+            <p>Price: <strong>USD ${props.price.toFixed(2)}</strong></p>
         </div>
     );
 };
