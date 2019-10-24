18fed16e2b709b30aedefa8e6e0c842dadf20f8b
Ryan Mok <mokryan17@gmail.com>
2019-10-23 21:21:47 -0500

    Implemented PURCHASE_BURGER_START in orders.js to switch loading to true; also refactored ContactData to work with redux state for loading

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -94,7 +94,6 @@ class ContactData extends Component {
             },
         },
         formIsValid: false,
-        loading: false
     };
 
     checkValidation = (value, rules) => {
@@ -200,7 +199,7 @@ class ContactData extends Component {
                 >ORDER</Button>
             </form>
         )
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />
         }
 
@@ -217,14 +216,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     };
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
