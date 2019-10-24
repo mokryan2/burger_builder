03276763f195c53c13cde8441e6b591122078a56
Ryan Mok <mokryan17@gmail.com>
2019-10-23 21:36:58 -0500

    Moved checkoutSummary into an if statement to handle the error caused by ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 class Checkout extends Component {
@@ -17,20 +17,26 @@ class Checkout extends Component {
 
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route
-                    path={this.props.match.path + "/contact-data"}
-                    component={ContactData}
-                />
-            </div>
-        )
-    }
+        let summary = <Redirect to="/" />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                    />
+                    <Route
+                        path={this.props.match.path + "/contact-data"}
+                        component={ContactData}
+                    />
+                </div>
+                // This was causing an error b/c ingredients was initially null; this means if we tried to loop through the ingredients it would fail causing said error.
+                // This way if the user tries to make his way to the checkout page before selecting ingredients, they'll be forcibly redirected to the landing page
+            )
+        };
+        return summary;
+    };
 };
 
 const mapStateToProps = state => {
