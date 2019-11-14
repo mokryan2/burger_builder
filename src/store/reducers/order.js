ea8b467d12f884c593d08eb5ddb4bf4126de72e2
Ryan Mok <mokryan17@gmail.com>
2019-11-13 23:31:44 -0600

    Refactored the order reducer to make it easier to read the switch statements being handled

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,5 +1,6 @@
 import * as actionTypes from "../actions/actionTypes";
 import { updateObject } from "../utility";
+import { stat } from "fs";
 
 const initialState = {
     orders: [],
@@ -7,38 +8,57 @@ const initialState = {
     purcahsed: false
 };
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, { purchased: false });
+};
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, { loading: true });
+};
+
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, { id: action.orderId });
+    // This order is actually recieved through the action, but the ID is on a seperate property; this will combine the two into one
+    return updateObject(
+        state,
+        {
+            loading: false,
+            purchased: true,
+            orders: state.orders.concat(newOrder)
+            // Reminder: concat() creates a new array that includes elements in object; maintains IMMUTABILITY!!!
+        });
+};
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, { loading: false });
+};
+
+const fetchOrdersStart = (state, action) => {
+    return updateObject(state, { loading: true });
+};
+
+const fetchOrdersSuccess = (state, action) => {
+    return updateObject(
+        state,
+        {
+            orders: action.orders,
+            loading: false
+        });
+};
+const fetchOrdersFail = (state, action) => {
+    return updateObject(state, { loading: false });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updateObject(state, { purchased: false });
-        case actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, { loading: true });
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = updateObject(action.orderData, { id: action.orderId });
-            // This order is actually recieved through the action, but the ID is on a seperate property; this will combine the two into one
-            return updateObject(
-                state,
-                {
-                    loading: false,
-                    purchased: true,
-                    orders: state.orders.concat(newOrder)
-                    // Reminder: concat() creates a new array that includes elements in object; maintains IMMUTABILITY!!!
-                });
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state, { loading: false });
-        case actionTypes.FETCH_ORDERS_START:
-            return updateObject(state, { loading: true });
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(
-                state,
-                {
-                    orders: action.orders,
-                    loading: false
-                });
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, {loading: false});
-        default:
-            return state
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action)
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
+        default: return state
     }
 };
 
