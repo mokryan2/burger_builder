a7a848fda74883a4c6e8292a3dcd9fe48b449c71
Ryan Mok <mokryan17@gmail.com>
2019-11-13 22:52:57 -0600

    Refactored the burgerBuilder Reducer to use the utility.js file to simplify the code

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
     ingredients: null,
@@ -17,26 +18,21 @@ const INGREDIENT_PRICE = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                // This is something that should be done in all reducers to maintain immutable state,
-                // HOWEVER, this won't create deep clones of an object; you need to go another level!!!
-                ingredients: {
-                    ...state.ingredients,
-                    // Need the extra level because spread operator won't go into objects to create new ones
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName]
-            };
+            }
+            return updateObject(state, updatedState);
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName]
-            };
+                const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+                const updatedIngs = updateObject(state.ingredients, updatedIng);
+                const updatedSt = {
+                    ingredients: updatedIngs,
+                    totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName]
+                }
+                return updateObject(state, updatedSt);
         case actionTypes.SET_INGREDIENTS:
             return {
                 ...state,
