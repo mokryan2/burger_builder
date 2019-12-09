2094dd86bc17b6a93171d8e3b047dccba336f6e1
Ryan Mok <mokryan17@gmail.com>
2019-12-08 22:46:47 -0600

    Refactored auth start into a funciton to make file more legible; additionally, created functions for success and fail

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -6,12 +6,47 @@ const initialState = {
     userId: null,
     error: null,
     loading: false
-}
+};
+
+const authStart = (state, action) => {
+    return updateObject(
+        state,
+        {
+            error: null,
+            loading: true
+        }
+    );
+};
+
+const authSuccess = (state, action) => {
+    return updateObject(
+        state,
+        {
+            token: action.idToken,
+            userId: action.userId,
+            loading: false
+        }
+    );
+};
+
+const authFail = (state, action) => {
+    return updateObject(
+        state,
+        {
+            error: action.error,
+            loading: false
+        }
+    );
+};
 
 const reducer = (state, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START:
-            return updateObject(state, { error: null, loading: true });
+            return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS:
+            return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL:
+            return authFail(state, action);
         default:
             return state;
     }
