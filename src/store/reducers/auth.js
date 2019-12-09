2e4bab501cff7653885ae72393dcda3cdd43a4c6
Ryan Mok <mokryan17@gmail.com>
2019-12-08 22:52:29 -0600

    Fixed error caused from not including initiatState as state in auth reudcer and included auth reducer into index.js file

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -39,7 +39,7 @@ const authFail = (state, action) => {
     );
 };
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START:
             return authStart(state, action);
