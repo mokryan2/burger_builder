8285e3e83d110096f7a678ba52b7ac2f511265e4
Ryan Mok <mokryan17@gmail.com>
2020-03-08 23:52:45 -0500

    Small UI update to force the side drawer to close when clicking anywhere/ on a link

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -17,7 +17,10 @@ const sideDrawer = (props) => {
                 show={props.open}
                 clicked={props.closed}
             />
-            <div className={attachClasses.join(" ")}>
+            <div
+                className={attachClasses.join(" ")}
+                onClick={props.closed}>
+                {/* By including the onClick method here, the side drawer will now automatically close regardless of clicking the screen or on a link */}
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
