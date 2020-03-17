88002ce1a7c15611cde3168f3a6d0c2776afdd11
Ryan Mok <mokryan17@gmail.com>
2020-03-17 11:26:41 -0500

    Implemented the updateObject method into the auth.js file

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import classes from "./Auth.css";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../../store/actions/index";
+import { updateObject } from "../../shared/utility";
 
 class Auth extends Component {
 
@@ -80,15 +81,13 @@ class Auth extends Component {
     };
 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidation(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
-        };
+            })
+        });
         this.setState({
             controls: updatedControls
         });
